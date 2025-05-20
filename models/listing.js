175f6af03988692c88b74b51cf04981ceb6546ee
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-05-20 08:41:38 +0530

    updated listing model

--- models/listing.js
+++ models/listing.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ListingSchema = new Schema ({
-    title :{
-      type :   String,
-      required : true
-    } ,
-    description : String,
-    image : {
-        type :   String,
-        default:"https://plus.unsplash.com/premium_photo-1746540729970-d4450055dbfc?q=80&w=3628&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set:(v)=> v === ""? "https://plus.unsplash.com/premium_photo-1746540729970-d4450055dbfc?q=80&w=3628&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
-      },
-    price : Number,
-    location : String,
-    country : String
-})
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+   
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
 
-const Listing = mongoose.model("Listing",ListingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
