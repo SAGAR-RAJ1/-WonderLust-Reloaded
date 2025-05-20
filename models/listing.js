6a25f1d428fca45905e6999d770452b7d61c36c1
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-05-20 08:40:56 +0530

    Added Initial Data in the Database

--- models/listing.js
+++ models/listing.js
@@ -9,6 +9,7 @@ const ListingSchema = new Schema ({
     description : String,
     image : {
         type :   String,
+        default:"https://plus.unsplash.com/premium_photo-1746540729970-d4450055dbfc?q=80&w=3628&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=> v === ""? "https://plus.unsplash.com/premium_photo-1746540729970-d4450055dbfc?q=80&w=3628&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
       },
     price : Number,
