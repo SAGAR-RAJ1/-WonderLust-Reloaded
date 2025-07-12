a37988fb35e80a9633862ad117ab98d968a57c90
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-07-12 07:38:05 +0530

    Used router.route for user.js in routes for further simplifying the code

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+module.exports.Signup= (req, res) => {
+  res.render("users/signup.ejs");
+};
+module.exports.Login= (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.PostSignup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUSer = new User({ email, username });
+    const registeredUser = await User.register(newUSer, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      // this is for automatically login after the sign-up
+      if (err) {
+        return next(err); // if there is an error, this will execute or if there is not an error the other code will execute normally
+      }
+      req.flash("success", "Welcome to WanderLust");
+      res.redirect("/listings");
+    });
+  } catch (error) {
+    res.send("Account exists go back to signup page");
+  }
+};
+module.exports.PostLogin = async (req, res) => {
+  //statergy    //failure k case m kaha redirect ho
+  req.flash("success", "Welcome to WanderLust");
+  res.redirect("/listings");
+};
+
+module.exports.Logout= (req, res, next) => {
+  req.logout((err) => {
+    //Passport k apna checking h logout krne k liye
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are logged out");
+    res.redirect("/listings");
+  });
+};
+
--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,48 @@
-const express = require('express');
-const router = express.Router({mergeParams:true});//merge params to get the data from the parent query
-const User= require("../models/user");
-const passport = require('passport');
+const express = require("express");
+const router = express.Router({ mergeParams: true }); //merge params to get the data from the parent query
+const User = require("../models/user");
+const passport = require("passport");
 
-router.get("/signup",(req,res)=>{
-  
-    res.render("users/signup.ejs")
-})
-router.get("/login",(req,res)=>{
-  
-    res.render("users/login.ejs")
-})
+const wrapasync = require("../utils/WrapAsync");
 
-router.post("/signup",async(req,res)=>{
+const ListingUser = require("../controllers/user");
 
-    try {
-        let {username,email,password}=req.body;
-        const newUSer = new User({email,username});
-        const registeredUser = await User.register(newUSer,password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err)=>{// this is for automatically login after the sign-up
-            if(err){
-                return next(err);// if there is an error, this will execute or if there is not an error the other code will execute normally
-            }
-            req.flash("success","Welcome to WanderLust");
-            res.redirect("/listings")
-        })
-     
-    } catch (error) {
-        
-     res.send("Account exists go back to signup page")
-    }
-   
-})                   //Middleware for checking users already exist or not
-router.post("/login",passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}) ,async(req,res)=>{
-                                           //statergy    //failure k case m kaha redirect ho
-    req.flash("success","Welcome to WanderLust");  
-    res.redirect("/listings")
 
-})
+// we are using router.route as we it is used to structure the code I say, but that's not all it's used For writing routes, whether it is get post delete if they have the same path, we can club them together with a single path. You will be shown below
 
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{//Passport k apna checking h logout krne k liye
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You are logged out");
-        res.redirect("/listings")
-    })
-})
+router.route("/signup")
+.get( ListingUser.Signup)
+.post(ListingUser.PostSignup); //Middleware for checking users already exist or not
 
-module.exports=router;
+
+router.route("/login")
+.get( ListingUser.Login)
+.post(passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  wrapasync(ListingUser.PostLogin)
+);
+
+router.route("/logout")
+.get(ListingUser.Logout);
+
+
+//todo old code without router.route
+// router.get("/signup", ListingUser.Signup);
+
+// router.get("/login", ListingUser.Login);
+
+// router.post("/signup",ListingUser.PostSignup); //Middleware for checking users already exist or not
+// router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     failureRedirect: "/login",
+//     failureFlash: true,
+//   }),
+//   wrapasync(ListingUser.PostLogin)
+// );
+
+// router.get("/logout",ListingUser.Logout) ;
+
+module.exports = router;
